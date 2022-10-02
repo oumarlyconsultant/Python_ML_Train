b732861966a7f83a84c34289f783f0c5f67f52d0
oumar <oumarly.consultant@gmail.com>
2022-10-02 11:35:44 -0400

    import libraries

--- TF_Decision_Trees_Forests.py
+++ TF_Decision_Trees_Forests.py
@@ -1,2 +1,11 @@
 #Using TF to train DT and RF
 #Tutorial link: https://www.tensorflow.org/decision_forests/tutorials/beginner_colab
+# !pip install tensorflow_decision_forests
+
+import tensorflow_decision_forests as tfdf
+
+import os
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+import math
